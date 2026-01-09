0072001a7fd9fac1fb787aeb3089e0796acabb4b
Flo <171354410+Nowaroid-Studios@users.noreply.github.com>
2026-01-09 20:04:28 +0100

    Add files via upload

---
+++ devroid/__init__.py
@@ -0,0 +1,37 @@
+"""
+devroid - A utility library for Pycord bots
+"""
+
+from .wizzycolor import (
+    Fore,
+    Style,
+    Color,
+    BOX_CHARS,
+    init,
+    create_exact_table,
+    create_exact_status_box,
+    display_cogs,
+    display_bot_interface,
+    create_message_line,
+    display_status_line
+)
+
+__version__ = "0.1.0"
+__author__ = "Your Name"
+
+__all__ = [
+    # Color classes
+    "Fore",
+    "Style",
+    "Color",
+    "BOX_CHARS",
+    
+    # Functions
+    "init",
+    "create_exact_table",
+    "create_exact_status_box",
+    "display_cogs",
+    "display_bot_interface",
+    "create_message_line",
+    "display_status_line",
+]
